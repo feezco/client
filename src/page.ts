21c71bd280197076772c9d2bbbe167ab663da091
Hafiz Al Faza <hafizalfaza@gmail.com>
2022-04-05 17:12:10 +0200

    feat: getPageContent

--- src/page.ts
+++ src/page.ts
@@ -1,3 +1,20 @@
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 export interface FeezcoPage {
-  elements: Record<string,unknown>;
+  elements: Record<string, unknown>;
 }
+
+export const getPageContent = async <T>({
+  path,
+  key,
+}: {
+  path: string;
+  key: string;
+}): Promise<T> => {
+  const res = (await axios.get(
+    `https://cdn.feezco.com?path=${path}&key=${key}&stage=${process.env.FEEZCO_STAGE}`
+  )) as { data: T };
+
+  return res.data;
+};
