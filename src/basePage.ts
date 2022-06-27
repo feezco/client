eb99db3bfc9a31cf2ded19cf3c0fe635eacfce1d
Hafiz Al Faza <hafizalfaza@gmail.com>
2022-06-27 21:13:23 +0200

    fix: fuck

--- src/basePage.ts
+++ src/basePage.ts
@@ -11,13 +11,13 @@ type PageContent<T> = T extends T ? T : T;
 export const getPageContent = async <T>({
   path,
   key,
-  env
+  env,
 }: {
   path: string;
   key?: string;
-  env?: string
+  env?: string;
 }): Promise<PageContent<T> | void> => {
-  if (window) {
+  if (typeof window !== "undefined" && typeof document !== "undefined") {
     const getCookie = (cname: string): string => {
       const name = cname + "=";
       const ca = document.cookie.split(";");
@@ -35,10 +35,10 @@ export const getPageContent = async <T>({
 
     const setCookie = (cname: string, cvalue: string, exdays: number) => {
       const d = new Date();
-      d.setTime(d.getTime() + (exdays*24*60*60*1000));
-      let expires = "expires="+ d.toUTCString();
+      d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+      let expires = "expires=" + d.toUTCString();
       document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    }
+    };
 
     const populateMissingElementsBrowser = ({
       pagePath,
@@ -66,98 +66,101 @@ export const getPageContent = async <T>({
       ).data as PageContent<T>);
 
     if (!cachedContentDataRes && resData) {
-      setCookie(
-        'feezco_cached',
-        JSON.stringify(resData),
-        100000
-      );
+      setCookie("feezco_cached", JSON.stringify(resData), 100000);
     }
 
     // @ts-expect-error type error
     return populateMissingElementsBrowser({ pagePath: path, data: resData });
   } else {
-    const { existsSync, readFileSync, writeFileSync } = await import("fs");
+    try {
+      const { existsSync, readFileSync, writeFileSync } = await import("fs");
 
-    const feezcoConfig = existsSync(`${process.cwd()}/feezco.config.json`)
-      ? readFileSync(`${process.cwd()}/feezco.config.json`, "utf-8")
-      : null;
+      const feezcoConfig = existsSync(`${process.cwd()}/feezco.config.json`)
+        ? readFileSync(`${process.cwd()}/feezco.config.json`, "utf-8")
+        : null;
 
-    if (!feezcoConfig) {
-      throw new Error(
-        "Cannot find feezco config file. Please make sure feezco.config.json is set up in your project root."
-      );
-    }
-
-    const feezcoPlaceholders = existsSync(
-      `${process.cwd()}/feezco.placeholders.json`
-    )
-      ? readFileSync(`${process.cwd()}/feezco.placeholders.json`, "utf-8")
-      : null;
-
-    const feezcoConfigParsed: {
-      pages: Record<string, { id: string; path: string }>;
-      key: string;
-    } = JSON.parse(feezcoConfig);
-
-    const feezcoPlaceholdersParsed: Record<string, string> = feezcoPlaceholders
-      ? JSON.parse(feezcoPlaceholders)
-      : null;
-
-    const populateMissingElements = ({
-      pagePath,
-      data,
-    }: {
-      pagePath: string;
-      data: Record<string, unknown>;
-    }) => {
-      const key = Object.keys(feezcoConfigParsed.pages).find(
-        (k) => feezcoConfigParsed.pages[k].path === pagePath
-      );
-
-      if (key && feezcoPlaceholdersParsed[key]) {
-        const missingElementsKeys = Object.keys(feezcoPlaceholdersParsed[key]);
-
-        for (const elementKey of missingElementsKeys) {
-          // @ts-expect-error
-          if (!data.elements[elementKey]) {
-            // @ts-expect-error
-            data.elements[elementKey] =
-              feezcoPlaceholdersParsed[key][elementKey as unknown as number];
-          }
-        }
+      if (!feezcoConfig) {
+        throw new Error(
+          "Cannot find feezco config file. Please make sure feezco.config.json is set up in your project root."
+        );
       }
 
-      return data;
-    };
+      const feezcoPlaceholders = existsSync(
+        `${process.cwd()}/feezco.placeholders.json`
+      )
+        ? readFileSync(`${process.cwd()}/feezco.placeholders.json`, "utf-8")
+        : null;
 
-    const cachedContentDataRes =
-      existsSync(`${__dirname}/cachedContentData.json`) &&
-      process.env.FEEZCO_STAGE !== "PRODUCTION"
+      const feezcoConfigParsed: {
+        pages: Record<string, { id: string; path: string }>;
+        key: string;
+      } = JSON.parse(feezcoConfig);
+
+      const feezcoPlaceholdersParsed: Record<string, string> =
+        feezcoPlaceholders ? JSON.parse(feezcoPlaceholders) : null;
+
+      const populateMissingElements = ({
+        pagePath,
+        data,
+      }: {
+        pagePath: string;
+        data: Record<string, unknown>;
+      }) => {
+        const key = Object.keys(feezcoConfigParsed.pages).find(
+          (k) => feezcoConfigParsed.pages[k].path === pagePath
+        );
+
+        if (key && feezcoPlaceholdersParsed[key]) {
+          const missingElementsKeys = Object.keys(
+            feezcoPlaceholdersParsed[key]
+          );
+
+          for (const elementKey of missingElementsKeys) {
+            // @ts-expect-error
+            if (!data.elements[elementKey]) {
+              // @ts-expect-error
+              data.elements[elementKey] =
+                feezcoPlaceholdersParsed[key][elementKey as unknown as number];
+            }
+          }
+        }
+
+        return data;
+      };
+
+      const cachedContentDataRes =
+        existsSync(`${__dirname}/cachedContentData.json`) &&
+        process.env.FEEZCO_STAGE !== "PRODUCTION"
+          ? JSON.parse(
+              readFileSync(`${__dirname}/cachedContentData.json`, "utf-8")
+            )
+          : null;
+
+      const feezcoConfigObj = existsSync(`${process.cwd()}/feezco.config.json`)
         ? JSON.parse(
-            readFileSync(`${__dirname}/cachedContentData.json`, "utf-8")
+            readFileSync(`${process.cwd()}/feezco.config.json`, "utf-8")
           )
         : null;
 
-    const feezcoConfigObj = existsSync(`${process.cwd()}/feezco.config.json`)
-      ? JSON.parse(readFileSync(`${process.cwd()}/feezco.config.json`, "utf-8"))
-      : null;
+      const resData =
+        cachedContentDataRes ||
+        ((
+          await axios.get(
+            `https://cdn.feezco.com/page?path=${path}&key=${feezcoConfigObj.key}&stage=${process.env.FEEZCO_STAGE}`
+          )
+        ).data as PageContent<T>);
 
-    const resData =
-      cachedContentDataRes ||
-      ((
-        await axios.get(
-          `https://cdn.feezco.com/page?path=${path}&key=${feezcoConfigObj.key}&stage=${process.env.FEEZCO_STAGE}`
-        )
-      ).data as PageContent<T>);
+      if (!cachedContentDataRes && resData) {
+        writeFileSync(
+          `${__dirname}/cachedContentData.json`,
+          JSON.stringify(resData)
+        );
+      }
 
-    if (!cachedContentDataRes && resData) {
-      writeFileSync(
-        `${__dirname}/cachedContentData.json`,
-        JSON.stringify(resData)
-      );
+      // @ts-expect-error type error
+      return populateMissingElements({ pagePath: path, data: resData });
+    } catch (err) {
+      // error
     }
-
-    // @ts-expect-error type error
-    return populateMissingElements({ pagePath: path, data: resData });
   }
 };
